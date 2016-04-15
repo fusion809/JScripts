b9e217877055d458851963164451afb21d9fa9e4
fusion809 <brentonhorne77@gmail.com>
2016-04-16 03:45:26 +1000

    updating scripts

--- math/RK4/airy.js
+++ math/RK4/airy.js
@@ -5,6 +5,9 @@
 // dy/dx = y[1]
 // xmax <= x <= x0
 
+// Import RK4 2nd order library
+var FUNCT = require('../../lib/RK4/2nd.js');
+
 // d2y/dx2 = f(x,y,dy/dx)
 function f(x, y, dy) {
   var d2y = x * y;
@@ -14,51 +17,7 @@ function f(x, y, dy) {
 var y0       = 0.355028053887817239260063186004183176397979174199177240583, // y[0]  = 0
     dy0      = -0.25881940379280679840518356018920396347909113835493458221, // dy[0] = 0
     x0       = 0,
-    xmax     = -250,
+    x1       = -250,
     N        = 1000001
 
-while (i < N) {
-    y           = rk4(x, h, y, dy, f)[0];
-    dy          = rk4(x, h, y, dy, f)[1];
-    Y[i+1]      = y;
-    dY[i+1]     = dy;
-    x           = x + h;
-    X[i+1]      = x;
-    i          += 1;
-}
-
-X[0]     = x0,
-Y[0]     = y0,
-dY[0]    = dy0
-
-var Yarr  = Object.keys(Y).map(function (key) {return [X[key], Y[key]]}); // inspired by http://stackoverflow.com/a/26166303/1876983
-var dYarr = Object.keys(dY).map(function (key) {return [X[key], dY[key]]}); // inspired by http://stackoverflow.com/a/26166303/1876983
-var Parr  = Object.keys(Y).map(function (key) {return [Y[key], dY[key]]}); // inspired by http://stackoverflow.com/a/26166303/1876983
-
-console.log(X[0])
-console.log(Y[N])
-
-function plot() {
-  gnuplot(0).plot([{
-      title:"y[x] vs. x",
-      style:"lines",
-      color:"#F0F",
-      data:Yarr
-  }]);
-
-  gnuplot(1).plot([{
-      title:"dy[x] vs. x",
-      style:"lines",
-      color:"#F0F",
-      data:dYarr
-  }]);
-
-  gnuplot(2).plot([{
-      title:"dy[x] vs. y[x]",
-      style:"lines",
-      color:"#F0F",
-      data:Parr
-  }]);
-}
-
-plot()
+FUNCT.RK4(x0, x1, y0, dy0, N, f)
